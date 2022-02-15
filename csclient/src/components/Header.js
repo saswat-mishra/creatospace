233e99a7abb20c51919d6703280f8c8df9eafe10
drifter <32086553+rahul237@users.noreply.github.com>
2022-02-15 23:05:26 +0530

    Update Header.js

--- csclient/src/components/Header.js
+++ csclient/src/components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomeIcon from "@material-ui/icons/Home";
+import Modal from "react-responsive-modal";
+import "react-responsive-modal/styles.css"
 import "./css/Header.css";
 function Header() {
+
+    const [isModalOpen, toggleModal] = useState(false)
+    const Close = ("Close")
+
     return (
         <div className='header'>
             <div className='headercontent'>
@@ -22,6 +28,26 @@ function Header() {
                     <div>Search Creatospace</div>
 
                 </div>
+                <div onClick={() => toggleModal(true)}>
+                    Add Question
+                </div>
+                <Modal open={isModalOpen} closeIcon={Close} onClose={()=>toggleModal(false)} closeOnEsc={true} center closeOnOverlayClick={false} >
+                    <div className='modal_title'>
+                        This is modal title
+                        <br></br>
+
+                        Enter question
+
+
+
+                        <input >
+                        </input>
+                        <br></br>
+                        Submit
+
+                    </div>
+                </Modal>
+                
             </div>
         </div>
     )
