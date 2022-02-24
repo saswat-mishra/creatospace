9e309c90847bbcd309cfa0c22c0fe986d8971231
rahul237 <rahulbhushanmehta@gmail.com>
2022-02-25 01:07:30 +0530

    login working

--- csclient/src/App.js
+++ csclient/src/App.js
@@ -1,13 +1,32 @@
 
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Creatospace from './components/Creatospace';
-import { selectUser } from './feature/userSlice';
+import { login, selectUser } from './feature/userSlice';
 import Login from './components/auth/Login.js'
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase';
+// import { auth } from './firebase';
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (authUser)=>{
+      if(authUser){
+        dispatch(login({
+          userName: authUser.displayName,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          uid : authUser.uid
+        }))
+        console.log("AuthUser",authUser)
+      }
+    })
+
+  }, [dispatch])
   return (
     <div className="App">
       {user ?   (<Creatospace/>) : (<Login/>)}
--- csclient/src/components/auth/Login.js
+++ csclient/src/components/auth/Login.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Login.css"
+import {signInWithPopup} from 'firebase/auth'
+import {auth, provider} from '../../firebase'
 
 function Login() {
+    const handleSubmit = async()=>{
+       await signInWithPopup(auth, provider).then((result)=>{
+           console.log(result)
+       }).catch((e)=>{
+           console.log(e)
+       })
+    }
     return (<div className="login_container">
         <div className="login_content">
-            {/* <img src="" alt="logo"></img> */}
-            <button className="login_button">Login  ddd</button>
+        <img src={require("../../images/logo.png")} alt='logo' />
+            <button onClick={handleSubmit} className="login_button">Login  ddd</button>
 
         </div>
 
